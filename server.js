72fc3978e76e896995569f28aa2ab3accb6e597e
Abdul Rahman <abdulrahmanmlg744@gmail.com>
2026-03-10 15:26:20 +0530

    Initial Node Project Setup

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT;
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('Hello from News AI')
